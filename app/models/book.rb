dfd9574b8c9e4da46ba175063f66f440bb42883a
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2011-11-17 23:16:15 +0900

    BookモデルにMemoとの関連を張った。

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+  has_many :memos
   validates :title, :presence => true, :length => { :maximum => 256 }
   scope :this_month, lambda{ where(:updated_at => (Time.now.beginning_of_month)..(Time.now.end_of_month)) }
 end
