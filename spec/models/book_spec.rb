0239ddddeb113c81e9be625b1fb8ef7ca67796b5
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2011-11-19 17:07:29 +0900

    課題: タイトルを256字以下に制限し、そのspecを書く

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -13,6 +13,18 @@ describe Book do
         subject { Book.new(:title => "ハリーポッター").valid? }
         it { should be_true }
       end
+
+      context "256文字以下の場合" do
+        title = "a" * 256
+        subject { Book.new(:title => title).valid? }
+        it { should be_true }
+      end
+
+      context "256文字超過の場合" do
+        title = "a" * (256 + 1)
+        subject { Book.new(:title => title).valid? }
+        it { should be_false }
+      end
     end
   end
 end
