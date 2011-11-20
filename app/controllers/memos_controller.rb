6a5cde881b106a439fac3e1d14384cba3bac3bcd
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2011-11-21 01:30:55 +0900

    memos コントローラをリファクタリング（フィルター：find_book）

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 class MemosController < ApplicationController
+  before_filter :find_book
+
   def new
-    @book = Book.find(params[:book_id])
     @memo = @book.memos.new
   end
 
   def create
-    @book = Book.find(params[:book_id])
     @memo = @book.memos.new(params[:memo])
     if @memo.save
       redirect_to book_path(@book), :notice => "メモを登録しました。"
@@ -16,12 +16,10 @@ class MemosController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:book_id])
     @memo = @book.memos.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:book_id])
     @memo = @book.memos.find(params[:id])
     if @memo.update_attributes(params[:memo])
       redirect_to book_path(@book), :notice => "メモを更新しました。"
@@ -31,9 +29,13 @@ class MemosController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
     @memo = @book.memos.find(params[:id])
     @memo.destroy
     redirect_to book_path(@book)
+  end
+
+  private
+  def find_book
+    @book = Book.find(params[:book_id])
   end
 end
