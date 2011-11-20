471ff03e6e8b07754a0d69234d3008889dea7317
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2011-11-21 01:27:39 +0900

    books コントローラをリファクタリング（フィルター）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class BooksController < ApplicationController
+  before_filter :find_book, :only => [:show, :destroy, :edit, :update]
 
   def new
     @book = Book.new
@@ -15,7 +16,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
   end
 
   def index
@@ -23,17 +24,17 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
 
   def edit
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
     if @book.update_attributes(params[:book])
       redirect_to @book, :notice => "更新しました。"
     else
@@ -41,4 +42,8 @@ class BooksController < ApplicationController
     end
   end
 
+  private
+  def find_book
+    @book = Book.find(params[:id])
+  end
 end
