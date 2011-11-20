70673898846d01b14c05b4abf59c90593b20e897
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2011-11-21 00:38:22 +0900

    Book を削除したときに、関連する Memo も削除されるようにした

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  has_many :memos
+  has_many :memos, :dependent => :destroy
   validates :title, :presence => true, :length => { :maximum => 256 }
   scope :this_month, lambda{ where(:updated_at => (Time.now.beginning_of_month)..(Time.now)) }
 end
